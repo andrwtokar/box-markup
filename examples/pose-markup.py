25a9f23ce21cea64d2b682e81acd47913f79cf58
Andrey Tokarew <tokarev.as@phystech.edu>
2023-12-18 01:27:57 +0300

    #1: Rename pose-murkup and add modeule to PYTHONPATH.

--- examples/pose-markup.py
+++ examples/pose-markup.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.append('./')
+
 import os
 from pose_markup.detector import PoseDetector
 from pose_markup.video_processor import VideoProcessor
 
-INPUT_PATH = os.path.join(os.path.curdir, "data/")
-OUTPUT_PATH = os.path.join(os.path.curdir, "output_data/")
+INPUT_PATH = os.path.join(os.getcwd(), "data/")
+OUTPUT_PATH = os.path.join(os.getcwd(), "output_data/")
 MODEL_PATH = os.path.join(os.getcwd(), "model/pose_landmarker_full.task")
 
 if not os.path.exists(OUTPUT_PATH):
@@ -13,6 +16,8 @@ detector = PoseDetector(MODEL_PATH)
 video_proc = VideoProcessor(detector, OUTPUT_PATH)
 
 names = os.listdir(INPUT_PATH)
+
+print(f"\n\n{names[::1]}")
 
 for name in names:
     video_proc.process_video(INPUT_PATH + name)
